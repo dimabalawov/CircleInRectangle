b5e94b204d45bc123d72de334f2f3f6223e742bd
dimabalawov <dimabalawov@gmail.com>
2023-10-14 21:06:22 +0300

    Добавьте файлы проекта.

---
+++ CircleInRectangle/CircleInRectangle.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+protected:
+    int side;
+public:
+    Rectangle() = default;
+    Rectangle(int s) :side(s) {};
+};
+class Circle
+{
+protected:
+    int radius;
+public:
+    Circle() = default;
+    Circle(int rad) :radius(rad) {};
+};
+class CircleInRectangle :public Rectangle, public Circle
+{
+public:
+    CircleInRectangle(int s, int rad)
+    {
+        radius = rad;
+        side = s;
+    }
+    void print()
+    {
+        cout << "Side of rectangle: " << side << endl;
+        cout << "Radius of circle: " << radius << endl;;
+    }
+};
+
+int main()
+{
+    CircleInRectangle a(20, 10);
+    a.print();
+}
+
